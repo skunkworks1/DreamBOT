548af60923228caf6a32b0f7dc330ef0ae0e493f
Dell Mobility Team - 1 <dellmobilityteam-1@dells-macbook-pro-2.local>
2015-12-02 16:59:12 -0600

    Update Appicon Asset folder

---
+++ ViewMaster/UIViewExtension.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewExtension.swift
+//  ViewMaster
+//
+//  Created by Dell Mobility Team - 1 on 11/24/15.
+//  Copyright © 2015 Dell. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    // Name this function in a way that makes sense to you...
+    // slideFromLeft, slideRight, slideLeftToRight, etc. are great alternative names
+    func slideInFromLeft(duration: NSTimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition animation
+        let slideInFromLeftTransition = CATransition()
+        
+        // Set its callback delegate to the completionDelegate that was provided (if any)
+        if let delegate: AnyObject = completionDelegate {
+            slideInFromLeftTransition.delegate = delegate
+        }
+        
+        // Customize the animation's properties
+        slideInFromLeftTransition.type = kCATransitionPush
+        slideInFromLeftTransition.subtype = kCATransitionFromLeft
+        slideInFromLeftTransition.duration = duration
+        slideInFromLeftTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        slideInFromLeftTransition.fillMode = kCAFillModeRemoved
+        
+        // Add the animation to the View's layer
+        self.layer.addAnimation(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
+    }
+}
